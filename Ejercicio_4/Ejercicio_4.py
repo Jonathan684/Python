1f9552848eca88a84cc4ef6de8fb500e13871776
Administrador <jonathan.lafalda@gmail.com>
2021-08-27 17:33:05 -0300

    Ejercicio_4

--- Ejercicio_4/Ejercicio_4.py
+++ Ejercicio_4/Ejercicio_4.py
@@ -1,38 +1,57 @@
-import random
 import módulo as md
-valor = round(random.uniform(1,2),2)
-print(valor)
 
-nombres = ["jonathan","nadia","cande"]
-nombre = random.choice(nombres)
-print(nombre)
-
-n=3
-Datos = n * [0]
-## registro == clase
-##Datos = 3 * [0]
-print(50*"--")
-Datos[0] = md.Alumnos("Manzana",20)
-Datos[1] = md.Alumnos("Pera",20)
-Datos[2] = md.Alumnos("Naranja",20)
-#i=0
-# while i != len(Datos)
-    #pritn(Datos[i])
-    #i +=1
-v = []
-v.append(2)
-v.append(3)
-v.append(1)
-print(v)
-for i in range(len(Datos)):
-    print(Datos[i])
-
-
-
-''''
-Recorrer todo --> for o while
-     mientras (condicion)
-    encontrar en mayor --> if
-
-print(Datos[0].fruta)
 '''
+Para calcular el promedio se debe sumar las cantidad de tiempo y 
+dividirlo en la cantidad de participantes.
+ T_N  5
+ T_CI 4
+ T_COR 1
+
+ TIMEPO PROMEDIO =  3/10 
+
+ Sumar los tiempo de una instancia del vector, ejemplo:
+ V[0].Tiempo_natación + V[0].Tiempo_ciclismo + V[0].Tiempo_corriendo = T_total
+
+ Cantidad_competencias = 3
+ promedio = Cantidad_competencias / T_total
+
+ Esto tambien se repite para v[1]. Se recomienda usar un ciclo for con un rango
+ igual al tamaño del vector (len(vector))
+ Anexo : El indice del ciclo for va a ser el indice para recorrer el vector.
+ i.e v[i]
+ '''
+def promedio(vector):
+    promedio_por_participante = len(vector) * [0]
+    Cantidad_competencias = 3
+    for i in range(len(vector)):
+        suma = vector[i].Tiempo_Natacion + vector[i].Tiempo_ciclismo + vector[i].Tiempo_corriendo
+        promedio_por_participante[i] = round((suma / Cantidad_competencias),2)
+    
+    return promedio_por_participante
+        
+
+
+###Cargar automatica
+primer_atleta = 0
+##TODO SE TIENE QUE VALIDAR
+def principal():
+    n = int(input("Ingrese la cantidad de atletas : "))
+    #validar(n)
+    vector = n * [0]
+    promedio_por_participante = n*[0]
+    #vector = cargar_datos_manual(vector)
+    vector = md.cargar_datos_automatico(vector)
+    promedio_por_participante  = promedio(vector)
+    promedio(vector)
+    for i in range(len(vector)):
+        print(vector[i])
+
+    for i in range(len(promedio_por_participante)):
+        print(promedio_por_participante[i])
+
+
+ 
+    
+
+if __name__ == '__main__':
+    principal()
--- Ejercicio_4/módulo.py
+++ Ejercicio_4/módulo.py
@@ -1,15 +1,39 @@
-class Alumnos:
-    def __init__(self, fruta, cantidad):
-        self.fruta = fruta
-        self.cantidad = cantidad
+
+'''
+#valor = round(random.uniform(1,2),2)
+#print(valor)
+#nombres = ["jonathan","nadia","cande"]
+#nombre = random.choice(nombres)
+
+'''
+import random
+class Atletas():
+    def __init__(self, Nomb, Tiempo_Nat, Tiempo_ciclismo, Tiempo_corriendo):
+        self.Nombre = Nomb
+        self.Tiempo_Natacion = Tiempo_Nat
+        self.Tiempo_ciclismo = Tiempo_ciclismo
+        self.Tiempo_corriendo = Tiempo_corriendo 
     def __str__(self):
-        s = self.fruta +" "+ (str(self.cantidad))
-        return s
-    def myfunction(self): ##metodo
-        return self.nombre
-'''
-clases:
-campos
-constructor
-metodos o funciones
-'''
+        cadena = self.Nombre + " " + str(self.Tiempo_Natacion) + " " + str(self.Tiempo_ciclismo) + " " + str(self.Tiempo_corriendo)
+        
+        #r += '{:<15}'.format('Codigo: '+str(self.codigo))
+        return cadena 
+
+def cargar_datos_manual(vector):
+    for i in range(len(vector)):
+        atleta = str(input("Ingrese nombre del atleta: "))
+        tiempo_n = float(input("Ingrese tiempo de natación: "))
+        tiempo_c = float(input("Ingrese tiempo ciclismo: "))
+        tiempo_corr = float(input("Ingrese tiempo corriendo: "))
+        vector[i] = md.Atletas(atleta, tiempo_n, tiempo_c, tiempo_corr)
+    return vector
+
+def cargar_datos_automatico(vector):
+    cadena = ("Nadia","Jonathan","Cande")
+    for i in range(len(vector)):
+        atleta = random.choice(cadena)
+        tiempo_n = round (random.uniform(1,4), 2)
+        tiempo_c = round (random.uniform(1,4), 2)
+        tiempo_corr = round (random.uniform(1,4), 2)
+        vector[i] = Atletas(atleta, tiempo_n, tiempo_c, tiempo_corr)
+    return vector
